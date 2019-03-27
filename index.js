af05d4e6efc7ebee065edab6cc688b699905f830
seccionp3 <seccionp3@gmail.com>
2019-03-27 15:00:10 -0500

    node mudules

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 var mongoose = require('mongoose');
 var app = require('./app');
-var port = process.env.PORT || 4444;
+var port = process.env.PORT || 3000;
 var uri = 'mongodb://oscar_admin:y2cglgn3ir3hzWAp@cluster0-shard-00-00-jcacl.mongodb.net:27017,cluster0-shard-00-01-jcacl.mongodb.net:27017,cluster0-shard-00-02-jcacl.mongodb.net:27017/dbwebaee?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true';
 mongoose.connect(uri, (err)=>{
     if(err){
